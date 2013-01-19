177edd1366d7a77f6bc9ec72fc3ead5e825668f2
Michael Peterson <Michael.Peterson@truestonefed.com>
2013-01-19 16:08:58 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyCodeBlog::Application.config.secret_token = '194f0353ac6a065b217f82bbf2b095e2462ea4b68e084d790ef8c3510aaa3c457a115792514d78920920e40545534fc3d70e8d913efe581e1cd8a915031fbc7a'
